233d3de10e1eaefb53e112849a858a0aa6b40d8b
Terje Sandstrom <terje@hermit.no>
2015-12-05 22:16:32 +0100

    Code for issue 88

---
+++ Issue88/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Issue88
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        class WTF
+        {
+            ~WTF() { throw new Exception(); }
+        }
+
+        WTF wtf;
+
+        [TestFixtureSetUp]
+        public void SU()
+        {
+            wtf = new WTF();
+        }
+
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(2, 2);
+        }
+
+        [TestFixtureTearDown]
+        public void TD()
+        {
+            wtf = null;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+    }
+}
