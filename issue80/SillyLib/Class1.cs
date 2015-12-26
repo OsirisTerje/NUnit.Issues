619a270baff9cc78c34a2b2931bba85d4d092e15
Terje Sandstrom <terje@hermit.no>
2015-12-26 23:42:47 +0100

    Added issue80

---
+++ issue80/SillyLib/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SillyLib
+{
+	public class Class1
+    {
+		List<string> _badList;
+
+		public async void DoIt()
+		{
+			await Task.Delay(4);
+			_badList.Add("this will die");
+		}
+    }
+
+
+}
---
+++ issue80/SillyTests/SillyTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using SillyLib;
+using System.Threading.Tasks;
+
+namespace SillyTests
+{
+	[TestFixture]
+	public class SillyTest
+	{
+
+		[Test]
+		public async Task WillItDie()
+		{
+			var sut = new Class1();
+
+			sut.DoIt();
+			await Task.Delay(20);
+
+			Assert.Fail();
+		}
+	}
+}
