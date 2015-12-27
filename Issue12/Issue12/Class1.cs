c5cdd980f496f6b93e1c8b43be259e7c5f9bcd1a
Terje Sandstrom <terje@hermit.no>
2015-12-27 21:58:05 +0100

    Added async tests for Issue12

---
+++ Issue12/Issue12/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace Issue12
+{
+    public class Class1
+    {
+        public async Task<int> AddAsync(int a, int b)
+        {
+            Task<int> insideAddTask = InsideAdd(a, b);
+            var res = await insideAddTask;
+            return res;
+        }
+
+        Task<int> InsideAdd(int x, int y)
+        {
+            return Task.Run(()=>x + y);
+        }
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ Issue12/Issue12/Issue12Tests/Class1Tests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using Issue12;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Issue12.Tests
+{
+    [TestFixture()]
+    public class Class1Tests
+    {
+        [Test]
+        public void AddTest()
+        {
+            var sut = new Class1();
+            var res = sut.Add(2, 3);
+            Assert.That(res, Is.EqualTo(5));
+        }
+
+        [Test]
+        public async void CheckAddAsync()
+        {
+            var sut = new Class1();
+            var result = await sut.AddAsync(2, 3);
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public async Task CheckAddAsyncWithTaskReturn()
+        {
+            var sut = new Class1();
+            var result = await sut.AddAsync(2, 3);
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public async Task CheckLambdas()
+        {
+            var sut = new Class1();
+            Assert.That(async () => await sut.AddAsync(2, 3), Is.EqualTo(5));
+            Assert.That(async () => await sut.AddAsync(42, 3), Is.EqualTo(45));
+        }
+
+    }
+}
