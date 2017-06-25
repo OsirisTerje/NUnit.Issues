71d480ce22a6054a82b9adbe133d1a1191e81a8b
Terje Sandstrom <terje@hermit.no>
2017-06-25 20:03:41 +0200

    Repro for Issue-83 in NUNit3. Can't repro problem any more.

---
+++ Issue83-Nunit3-2/Issue83-Nunit3-2/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Issue83_Nunit3_2
+{
+    public class TestIt
+    {
+        [Timeout(1000)]
+        [Test]
+        public void Run()
+        {
+            while (true)
+            { }
+            Assert.Pass();
+        }
+    }
+}
