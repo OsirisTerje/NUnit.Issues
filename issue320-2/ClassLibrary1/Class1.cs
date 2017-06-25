6f63004d4738a5433c73599495c3c995023d6a95
Terje Sandstrom <terje@hermit.no>
2017-06-25 21:18:45 +0200

    Added more

---
+++ issue320-2/ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        [Test]
+        public void Testit()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
