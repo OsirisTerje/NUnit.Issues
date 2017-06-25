db374e84cff52ae884a5e79452b691eda440d162
Terje Sandstrom <terje@hermit.no>
2017-06-25 16:42:58 +0200

    Issue-336, Categories - does not repro

---
+++ Issue336/Issue336/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Issue336
+{
+    [Category("OnClass")]
+    public class Outer
+    {
+        [Category("OnMethod")]
+        [Test]
+        public void TestIt()
+        {
+            Assert.Pass();
+        }
+    }
+
+
+    [Category("OnClassAndMethod")]
+    public class Outer2
+    {
+        [Category("OnClassAndMethod")]
+        [Test]
+        public void TestIt2()
+        {
+            Assert.Pass();
+        }
+    }
+}
