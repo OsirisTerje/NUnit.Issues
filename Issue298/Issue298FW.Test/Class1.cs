37ba7df3562018906dcb90c6e03908faef91d50e
Terje Sandstrom <terje@hermit.no>
2017-06-28 00:04:39 +0200

    Added random test

--- Issue298/Issue298FW.Test/Class1.cs
+++ Issue298/Issue298FW.Test/Class1.cs
@@ -16,5 +16,15 @@ namespace Issue298FW.Test
             var res = sut.Add(2, 3);
             Assert.That(res, Is.EqualTo(5));
         }
+
+        [Test]
+        public void TestRandom([Random(1, 1000, 10)] int x)
+        {
+            var sut = new Issue298.Math();
+            var res = sut.Add(x, 42);
+            Assert.That(res,Is.EqualTo(x+42));
+
+
+        }
     }
 }
